758c781f463280f27a8e2e51f493207387187140
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-09-01 17:43:58 +0200

    [FEATURE] Display tree in list view as well
    Resolves #55

--- Classes/Grid/RelationRenderer.php
+++ Classes/Grid/RelationRenderer.php
@@ -14,9 +14,11 @@ namespace Fab\Vidi\Grid;
  * The TYPO3 project - inspiring people to share!
  */
 
+use Fab\Vidi\Domain\Repository\ContentRepositoryFactory;
 use TYPO3\CMS\Backend\Utility\IconUtility;
 use Fab\Vidi\Domain\Model\Content;
 use Fab\Vidi\Tca\Tca;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class rendering relation
@@ -54,17 +56,74 @@ class RelationRenderer extends ColumnRendererAbstract {
 		} elseif ($table->field($this->fieldName)->hasMany()) {
 
 			if (!empty($this->object[$this->fieldName])) {
-				$template = '<li><a href="%s" data-uid="%s" class="btn-edit invisible">%s</a><span>%s</span></li>';
 
-				/** @var $foreignObject \Fab\Vidi\Domain\Model\Content */
-				foreach ($this->object[$this->fieldName] as $foreignObject) {
-					$result .= sprintf($template,
-						$this->getEditUri($foreignObject),
-						$this->object->getUid(),
-						IconUtility::getSpriteIcon('actions-document-open'),
-						$foreignObject[$foreignLabelField]);
+				$dataType = $this->object->getDataType();
+
+				if (Tca::table($dataType)->field($this->fieldName)->isTree()) {
+
+					$relatedDataType = Tca::table($this->object->getDataType())->field($this->fieldName)->getForeignTable();
+
+					// Initialize the matcher object.
+					/** @var \Fab\Vidi\Persistence\Matcher $matcher */
+					$matcher = GeneralUtility::makeInstance('Fab\Vidi\Persistence\Matcher', array(), $relatedDataType);
+
+					// Default ordering for related data type.
+					$defaultOrderings = Tca::table($relatedDataType)->getDefaultOrderings();
+					/** @var \Fab\Vidi\Persistence\Order $order */
+					$defaultOrder = GeneralUtility::makeInstance('Fab\Vidi\Persistence\Order', $defaultOrderings);
+
+					// Fetch related contents
+					$relatedContents = ContentRepositoryFactory::getInstance($relatedDataType)->findBy($matcher, $defaultOrder);
+
+
+					$fieldConfiguration = Tca::table($dataType)->field($this->fieldName)->getConfiguration();
+					$parentField = $fieldConfiguration['treeConfig']['parentField'];
+
+					$flatTree = array();
+					foreach ($relatedContents as $node) {
+						$flatTree[$node->getUid()] = array(
+							'item' => $node,
+							'parent' => $node[$parentField] ? $node[$parentField]['uid'] : NULL,
+
+						);
+					}
+
+					$tree = array();
+
+					// If leaves are selected without its parents selected, those are shown as parent
+					foreach ($flatTree as $id => &$flatNode) {
+						if (!isset($flatTree[$flatNode['parent']])) {
+							$flatNode['parent'] = NULL;
+						}
+					}
+
+					foreach ($flatTree as $id => &$node) {
+						if ($node['parent'] === NULL) {
+							$tree[$id] = &$node;
+						} else {
+							$flatTree[$node['parent']]['children'][$id] = &$node;
+						}
+					}
+
+					$relatedContents = $tree;
+
+					$result = 'todo: format content';
+
+				} else {
+					$template = '<li><a href="%s" data-uid="%s" class="btn-edit invisible">%s</a><span>%s</span></li>';
+
+					/** @var $foreignObject \Fab\Vidi\Domain\Model\Content */
+					foreach ($this->object[$this->fieldName] as $foreignObject) {
+						$result .= sprintf($template,
+							$this->getEditUri($foreignObject),
+							$this->object->getUid(),
+							IconUtility::getSpriteIcon('actions-document-open'),
+							$foreignObject[$foreignLabelField]);
+					}
+
+					$result = sprintf('<ul class="unstyled">%s</ul>', $result);
 				}
-				$result = sprintf('<ul class="unstyled">%s</ul>', $result);
+
 			}
 		}
 		return $result;
